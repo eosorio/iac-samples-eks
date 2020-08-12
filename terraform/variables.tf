a3c942e614bca53408a7f0a7435280140e79c2cb
Eduado Osorio <Eduado.Osorio.IT@gmail.com>
2020-08-11 22:11:14 -0500

    Adding route table and associations

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,6 +11,27 @@ variable "aws_profile" {}
 variable "aws_shared_credentials_file" {}
 variable "aws_region" {}
 
+variable "cluster-name" {
+  default = "EKS-Sandbox"
+  type    = string
+}
+
+variable "asg_capacity" {
+  default = 1
+  type    = number
+}
+
+variable "asg_min_size" {
+  default = 1
+  type    = number
+}
+
+variable "asg_max_size" {
+  default = 1
+  type    = number
+}
+
+
 # VPC
 variable "vpc_cidr" {}
 variable "vpc_id" {}
@@ -18,8 +39,9 @@ variable "vpc_default_route_table_id" {}
 
 # Networking
 variable "subnet_cidrs" {
-    type = list(string)
+  type = list(string)
 }
 variable "subnet_id" {
   type = list(string)
 }
+
